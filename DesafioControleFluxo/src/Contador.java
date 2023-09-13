e17d2a14c8e360c5b2e2dc4672a698fb6ac3a011
Sthefany Serrao <142641365+serrsthefany@users.noreply.github.com>
2023-09-13 15:16:45 -0300

    ControleDeFluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,27 @@
+
+public class Contador {
+    public static void main(String[] args) {
+        int parametroUm = 12;
+        int parametroDois = 30;
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
